36c01e9afe758ce2097914079725fb6c00b7055e
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-11-03 13:27:12 +0100

    added base router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Layout from "pages/Layout/Layout";
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
